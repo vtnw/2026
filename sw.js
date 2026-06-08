30f6d7947605c0f5aae8858ee948eff98cc22ee0
vtnw <vtnw@users.noreply.github.com>
2026-06-08 15:33:48 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
 <script>
-    self.addEventListener("fetch", (event) => navigator.storage.getDirectory().then(root => root.getFileHandle(event.request, { create: true }).then(h => h.getFile().then(f => event.respondWith(f)))));
+    self.addEventListener("fetch", (event) => {
+        navigator.storage.getDirectory().then(root => {
+            root.getFileHandle(event.request, { create: true }).then(h => {
+                h.getFile().then(f => {
+                    event.respondWith(f)
+                });
+            });
+        });
+    });
 </script>
