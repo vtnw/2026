05cb82c269ecc133f233d20f1c4e332fa3521181
vtnw <vtnw@users.noreply.github.com>
2026-06-08 16:40:25 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,10 @@
-self.addEventListener("fetch", event => {
-    const response = await serve(event.request.url.slice(event.request.url.lastIndexOf("/") + 1));
-    
-    event.respondWith(response ?? fetch(event.request));
-});
+self.addEventListener("fetch", event => event.respondWith(handle(event)));
 
-serve = async (name) => {
+handle = async (event) => {
     const root = await navigator.storage.getDirectory();
-    const fileHandle = await root.getFileHandle(name, { create: true });
+    const fileHandle = await root.getFileHandle(event.request.url.slice(event.request.url.lastIndexOf("/") + 1), { create: true });
     const file = await fileHandle.getFile();
     const text = await file.text();
 
-    return !!text ? new Response(text, { headers: { "Content-Type": "text/html; charset=utf-8" } }) : null;
+    return !!text ? new Response(text, { headers: { "Content-Type": "text/html; charset=utf-8" } }) : fetch(event.request);
 }
