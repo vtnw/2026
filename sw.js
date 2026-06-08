371b24da54f0914c2c142774a8662de546b462ae
vtnw <vtnw@users.noreply.github.com>
2026-06-08 17:31:24 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,8 +2,9 @@ self.addEventListener("fetch", event => event.respondWith(process(event)));
 
 process = async event => {
     try {
+        const name = event.request.url.slice((new URL(event.request.url)).pathname.split('/').pop());
         const root = await navigator.storage.getDirectory();
-        const fileHandle = await root.getFileHandle(event.request.url.slice(event.request.url.lastIndexOf("/") + 1));
+        const fileHandle = await root.getFileHandle(name);
         const file = await fileHandle.getFile();
         const text = await file.text();
 
