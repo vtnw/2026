810da4c7878eaa016ac572ddb8f3154701d0c7cf
vtnw <vtnw@users.noreply.github.com>
2026-06-09 09:49:28 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,8 @@
-self.addEventListener("fetch", event => event.respondWith(process(event)));
+self.addEventListener("fetch", event => event.respondWith(process(event.request.url)));
 
-process = async event => {
-    try {
-        const name = new URL(event.request.url).pathname.split("/").pop();
-        const root = await navigator.storage.getDirectory();
-        const handle = await root.getFileHandle(name);
-        const file = await handle.getFile();
-        const text = await file.text();
-
-        return new Response(text, { headers: { "Content-Type": "text/html; charset=utf-8" } });
-    } catch {
-        return fetch("local.html");
-    }
-}
+process = url => new Promise((resolve) => navigator.storage.getDirectory()
+    .then(r => r.getFileHandle(new URL(url).pathname.split("/").pop())
+        .then(h => h.getFile()
+            .then(f => f.text()
+                .then(t => resolve(new Response(text, { headers: { "Content-Type": "text/html; charset=utf-8" } }))))))
+    .catch(() => resolve(fetch("local.html"))));
